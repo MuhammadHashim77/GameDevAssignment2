e12e58e62ef6aaf0158ed3bc12519f338800cd47
MuhammadHashim77 <hashimtariq.work@gmail.com>
2023-07-09 22:04:07 +0500

    Cinemachine and Movement
    Added cinemachine to follow player and added movement to character

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Vector3 movement;
+    [SerializeField] private float movementSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(horizontal, 0f, vertical) * movementSpeed * Time.deltaTime;
+
+        transform.position += movement;
     }
 }
