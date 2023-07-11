c0aa8e52e8b7ed8b48d0fda44db9a475ed143cea
MuhammadHashim77 <hashimtariq.work@gmail.com>
2023-07-11 20:55:03 +0500

    Added running animation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,20 +6,41 @@ public class Movement : MonoBehaviour
 {
     Vector3 movement;
     [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 720f;
+    [SerializeField] private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        CharacterMovementRotation();
+    }
+
+    private void CharacterMovementRotation()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        movement = new Vector3(horizontal, 0f, vertical) * movementSpeed * Time.deltaTime;
+        movement = new Vector3(horizontal, 0f, vertical);
+        movement.Normalize();
 
-        transform.position += movement;
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+
+        if(movement != Vector3.zero)
+        {
+            animator.SetBool("Running", true);
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
     }
+
 }
